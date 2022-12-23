5d928ecd074e5aa1e203ff219ab8d1e5ccea9113
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-12-23 07:24:44 -0600

    Remove placeholder counter section

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import blockReducer from '../features/block/blockSlice';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     block: blockReducer,
   },
 });
