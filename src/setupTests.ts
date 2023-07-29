d6aaa950842f612badefa57ff77cd1305f89e1c9
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-07-29 10:15:58 -0400

    Add image snapshotting

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,10 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
 
 expect.extend(matchers);
+expect.extend({ toMatchImageSnapshot });
 
 afterEach(() => {
   cleanup();
