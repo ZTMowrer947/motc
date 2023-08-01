bef721a4c3feedd7552c244b5dda0b044d4d8310
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-01 15:13:29 -0400

    Add app rotation tests for remaining pieces

--- src/App.specdata.ts
+++ src/App.specdata.ts
@@ -19,4 +19,29 @@ export const appRotateTestData: AppRotateTestEntry[] = [
     rotations: 1,
     altTestName: 'should not change position of O-piece',
   },
+  {
+    type: 'T',
+    zeta: 'X/X/X/X/X/X/X/X/5A4/4AAA3/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 T 0 - 3 SLJZZIOTLSJIO',
+    rotations: 4,
+  },
+  {
+    type: 'S',
+    zeta: 'X/X/X/X/X/X/X/X/5AA3/4AA4/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 S 0 - 3 LJZZIOTLSJIOT',
+    rotations: 4,
+  },
+  {
+    type: 'L',
+    zeta: 'X/X/X/X/X/X/X/X/6A3/4AAA3/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 L 0 - 3 JZZIOTLSJIOTS',
+    rotations: 4,
+  },
+  {
+    type: 'J',
+    zeta: 'X/X/X/X/X/X/X/X/4A5/4AAA3/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 J 0 - 3 ZZIOTLSJIOTSL',
+    rotations: 4,
+  },
+  {
+    type: 'Z',
+    zeta: 'X/X/X/X/X/X/X/X/4AA4/5AA3/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 Z 0 - 3 ZIOTLSJIOTSL',
+    rotations: 4,
+  },
 ];
