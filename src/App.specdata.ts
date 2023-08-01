dfa9c39c1d89c363ec9f80b32a683f29ba32d81a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-01 14:32:11 -0400

    Add app rotation tests for O-piece

--- src/App.specdata.ts
+++ src/App.specdata.ts
@@ -13,4 +13,10 @@ export const appRotateTestData: AppRotateTestEntry[] = [
     zeta: 'X/X/X/X/X/X/X/X/3AAAA3/X/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 I 0 - 3 OTSLJZZIOTLSJ',
     rotations: 4,
   },
+  {
+    type: 'O',
+    zeta: 'X/X/X/X/X/X/X/X/4AA4/4AA4/X/X/X/X/X/X/X/X/X/O2OOOO3/OOOOOOOOO1 O 0 - 3 TSLJZZIOTLSJI',
+    rotations: 1,
+    altTestName: 'should not change position of O-piece',
+  },
 ];
